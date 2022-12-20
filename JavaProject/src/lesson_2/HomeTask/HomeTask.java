17471f9f39ef66b1e68b85561d372f924c238077
AleksandraKras <krasnozhenae@gmail.com>
2022-12-21 00:13:19 +0200

    Create HomeTask.java

---
+++ JavaProject/src/lesson_2/HomeTask/HomeTask.java
@@ -0,0 +1,48 @@
+package lesson_2.HomeTask;
+
+public class HomeTask {
+public static void main(String[] args) {
+byte Mybite;
+Mybite = -100;
+byte Mybite1;
+Mybite1 = 100;
+System.out.println(Mybite);
+System.out.println(Mybite1);
+
+int Myint;
+Myint=-10000000;
+int Myint1;
+Myint1=10000000;
+System.out.println(Myint);
+System.out.println(Myint1);
+
+long Mylong;
+Mylong=333333333333333333L;
+long Mylong2;
+Mylong2=111111111111111111L;
+System.out.println(Mylong);
+System.out.println(Mylong2);
+
+float Myfloat =-3.14f;
+float Myfloat2 = 3.1415f;
+System.out.println(Myfloat);
+System.out.println(Myfloat2);
+
+double Mydouble = -3.1414159265;
+double Mydouble1 = 3.1414159265;
+System.out.println(Mydouble);
+System.out.println(Mydouble1);
+
+boolean IsFalse = false;
+boolean IsTrue = true;
+System.out.println(IsFalse);
+System.out.println(IsTrue);
+
+char Mychar = '0';
+char Mychar1 = 'A';
+char Mychar2 = '\u2615';
+    System.out.println(Mychar);
+    System.out.println(Mychar1);
+    System.out.println(Mychar2);
+    }
+}
